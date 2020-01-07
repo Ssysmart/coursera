439dddba211bb3b6f7d8b3b6814cc1712dc17a49
Ssysmart <58028050+Ssysmart@users.noreply.github.com>
2020-01-07 17:16:28 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -300,3 +300,64 @@ for(int i = last - maxlen; i < last; i++)
 
 
 
+
+描述
+在main函数中， 生成一个5*5的矩阵，输入矩阵数据，并输入n，m的值。判断n，m是否在数组范围内，如果不在，则输出error；如果在范围内，则将n行和m行交换，输出交换n，m后的新矩阵。
+
+输入
+5*5矩阵的数据，以及n和m的值。
+
+输出
+如果不可交换，则输出error
+
+如果可交换，则输出新矩阵
+
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+
+int main() {
+
+int a[5][5] = {0};
+
+   for (int i = 0; i < 5; i++){
+	   for(int j = 0; j < 5; j++){
+		   int num;
+		   cin >> num;
+		   a[i][j] = num;
+	    }
+     }
+
+int n,m;
+
+cin>> n>>m;
+
+int interchange;
+
+
+   if(n < 5 && m < 5&& n >= 0&& m >=0){
+      for(int i = 0; i < 5; i++){
+      interchange = a[n][i];
+      a[n][i] = a[m][i];
+      a[m][i] = interchange;
+      }
+
+     for(int i = 0; i < 5; i++){
+    	 for(int j = 0; j < 5; j++){
+    		cout << setw(4)<<a[i][j];
+    	 }
+    	 cout<<endl;
+     }
+
+   }
+   else
+	   cout << "error"<<endl;
+  return 0;
+}
+
+
+
+
+
