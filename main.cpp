7520916bfb44bf62ed030ccab5e6fc8daa6f1168
Ssysmart <58028050+Ssysmart@users.noreply.github.com>
2020-01-17 20:05:54 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -517,3 +517,56 @@ int main() {
 
 
 
+描述
+已知一个整数数组x[],其中的元素彼此都不相同。找出给定的数组中是否有一个元素满足x[i]=i的关系，数组下标从0开始。
+
+举例而言，如果x[]={-2,-1,7,3,0,8},则x[3] = 3,因此3就是答案。
+
+输入
+第一行包含一个整数n (0 < n < 100)，表示数组中元素的个数。<br />第二行包含n个整数，依次表示数组中的元素。
+
+输出
+输出为一个整数，即满足x[i]=i的元素，若有多个元素满足，输出第一个满足的元素。若没有元素满足，则输出“N”。
+
+
+#include <iostream>
+using namespace std;
+int list[30]={0};
+
+int compare(int a, int b){
+	if(a == b)
+		return 1;
+	else
+		return 0;
+	}
+
+
+int main() {
+	int n;
+	cin>>n;
+
+	for(int i = 0; i < n; i++){
+		cin>>list[i];
+	}
+
+	int count = 0;
+	for(int j = 0; j < n; j++){
+		++count;
+		int result = 0;
+		result = compare(list[j], j);
+		if(result == 1) {
+			cout<<list[j]; break;
+		}
+		else
+			if(count == n &&compare(list[j], j) == 0 )
+				cout<<'N';
+	}
+
+
+return 0;
+}
+
+
+
+
+
