a70ea54008a72a1c74d583001e42cd31deb5ba8d
Ssysmart <58028050+Ssysmart@users.noreply.github.com>
2020-01-21 13:15:30 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -820,3 +820,86 @@ if(initial_month==month&&month!=2&&month>=1){
 }
 
 
+描述
+
+有一批易感人群住在网格状的宿舍区内，宿舍区为n*n的矩阵，每个格点为一个房间，房间里可能住人，也可能空着。在第一天，有些房间里的人得了流感，以后每天，得流感的人会使其邻居传染上流感，（已经得病的不变），空房间不会传染。请输出第m天得流感的人数。
+
+
+输入
+
+第一行一个数字n，n不超过100，表示有n*n的宿舍房间。 
+
+接下来的n行，每行n个字符，’.’表示第一天该房间住着健康的人，’#’表示该房间空着，’@’表示第一天该房间住着得流感的人。 
+
+接下来的一行是一个整数m，m不超过100.
+
+
+输出
+
+输出第m天，得流感的人数
+
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+char room[100][100];
+int  room_new[100][100] = {0};//记录新感染的人
+
+void chuanran(int n){
+	for(int i = 0; i<n; i++){
+		for(int j = 0; j<n;j++){
+			if(room[i][j] == '@'&&room_new[i][j] == 1){
+				if(room[i-1][j]=='.'&& i-1>=0){
+					room[i-1][j] = '@';
+				}
+				if(room[i+1][j] == '.'&& i+1<n){
+					room[i+1][j] = '@';
+				}
+				if(room[i][j+1] == '.'&& j+1<n){
+					room[i][j+1] = '@';
+				}
+				if(room[i][j-1] == '.' && j-1>=0){
+					room[i][j-1] = '@';
+				}
+				room_new[i][j] = 2;
+			}
+		}
+	}
+}
+
+int main() {
+        int n;
+        cin>>n;
+
+        for(int i = 0; i<n;i++){
+	      for(int j = 0; j<n; j++){
+	    	  cin>>room[i][j];
+	      }
+        }
+        int m;
+        cin>>m;
+        while(m>1){
+        	for(int i = 0; i<n;i++){
+        		for(int j = 0; j<n;j++){
+        			if(room[i][j] == '@'&&room_new[i][j] == 0){
+        				room_new[i][j] = 1;
+        			}
+        		}
+        	}
+        chuanran(n);
+
+        m--;
+        }
+ int sum = 0;
+        for(int i = 0;i<n;i++){
+        	for(int j = 0;j<n;j++){
+        		if(room[i][j] == '@'){
+        			sum++;
+        		}
+        	}
+        }
+        cout<<sum<<endl;
+
+	return 0;
+}
+
