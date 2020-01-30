971094ccc4f874ff2e4da277358bf012825aa657
Ssysmart <58028050+Ssysmart@users.noreply.github.com>
2020-01-30 19:06:54 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1122,3 +1122,70 @@ int main() {
    }
 
 
+描述
+输入一个整数矩阵，计算位于矩阵边缘的元素之和。所谓矩阵边缘的元素，就是第一行和最后一行的元素以及第一列和最后一列的元素。
+
+输入
+第一行为整数k，表示有k组数据。
+
+每组数据有多行组成，表示一个矩阵：
+
+第一行分别为矩阵的行数m和列数n（m < 100，n < 100），两者之间以空格分隔。
+
+接下来输入的m行数据中，每行包含n个整数，整数之间以空格作为间隔。
+
+输出
+
+输出对应矩阵的边缘元素和，一个一行。
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int sum = 0;
+int a[101][101]= {0};
+
+
+int main() {
+    int n;
+
+	cin>>n;
+	int m1,m2;
+
+
+	while(n>0){
+		cin>>m1>>m2;
+
+
+
+	    for(int i = 0; i<m1; i++){
+		   for(int j = 0; j<m2;j++){
+				cin>>a[i][j];
+			    }
+			}
+				int *p = a[0];
+		for(int i = 0; i<m1;i++){
+		 if(i==0){
+			 for(int j = 0; j<m2; j++){
+				 sum+=*p; p++;
+			 }
+
+		 }
+		 else if(i == m1-1){
+			 p = a[m1-1];
+			 for(int j = 0; j<m2; j++){
+				 sum+= *p;p++;
+			 }
+		 }
+		 else{
+			 sum+=(a[i][0] + a[i][m2-1]);
+		 }
+
+		}
+		cout<<sum<<endl;
+		sum = 0;
+		n--;
+	}
+
+
+    return 0;
+}
