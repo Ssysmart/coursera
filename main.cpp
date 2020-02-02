c6b36c46796056eedde21ce490e4c40d3a60beac
Ssysmart <58028050+Ssysmart@users.noreply.github.com>
2020-02-02 13:28:06 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1189,3 +1189,54 @@ int main() {
 
     return 0;
 }
+
+描述
+给定一个row行col列的整数数组array，要求从array[0][0]元素开始，按从左上到右下的对角线顺序遍历整个数组。
+
+
+输入
+
+输入的第一行上有两个整数，依次为row和col。
+
+余下有row行，每行包含col个整数，构成一个二维整数数组。
+
+（注：输入的row和col保证0 < row < 100, 0 < col < 100）
+
+输出
+按遍历顺序输出每个整数。每个整数占一行。
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+int main()
+{
+    int A[100][100];
+    int row, col, i, j, r=0, c=0;
+    cin >> row >> col;
+    for (i=0; i<row; i++)
+        for (j=0; j<col; j++)
+            cin >> A[i][j];
+
+
+   for(int j = 0; j<col;j++){
+	   r = 0;
+	   c = j;
+	   while(r<row&&c>=0){
+		   cout<<A[r++][c--]<<endl;;
+	   }
+   }
+
+   for(int i = 1; i<row; i++){
+	   c = col-1;
+	   r = i;
+	   while(c>=0&&r<=row-1){
+		   cout<<A[r++][c--]<<endl;;
+	   }
+   }
+
+
+    return 0;
+}
+
+
+
