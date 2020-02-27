4f572115313a8b220a3ac6a041d76dbb649d887a
Ssysmart <58028050+Ssysmart@users.noreply.github.com>
2020-02-27 11:17:01 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1479,5 +1479,64 @@ int main()
 }
 
 
+描述
+在一个学生信息处理程序中，要求实现一个代表学生的类，并且所有成员变量都应该是私有的。
 
+（注：评测系统无法自动判断变量是否私有。我们会在结束之后统一对作业进行检查，请同学们严格按照题目要求完成，否则可能会影响作业成绩。）
+
+输入
+姓名，年龄，学号，第一学年平均成绩，第二学年平均成绩，第三学年平均成绩，第四学年平均成绩。
+
+其中姓名、学号为字符串，不含空格和逗号；年龄为正整数；成绩为非负整数。
+
+各部分内容之间均用单个英文逗号","隔开，无多余空格。
+
+输出
+一行，按顺序输出：姓名，年龄，学号，四年平均成绩（向下取整）。
+
+各部分内容之间均用单个英文逗号","隔开，无多余空格。
+
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+//评测系统
+class student{
+private:
+	char name[20];
+	char Id[20];
+	int Age;
+	int grade;
+	int A,B,C,D;
+public:
+	int averagescore();
+	void basicinfo();
+	void getinfo();
+};
+int student::averagescore(){
+	return ((A+B+C+D)/4);
+}
+void student::getinfo(){
+	char a,b,c;
+	cin.getline(name,20,',');
+	cin>>Age>>a;
+	cin.getline(Id,20,',');
+	cin>>A>>a>>B>>b>>C>>c>>D;
+
+}
+void student:: basicinfo(){
+	cout<<name<<','<<Age<<','<<Id<<','<<averagescore();
+}
+
+int main()
+{
+
+student r;
+r.getinfo();
+r.basicinfo();
+
+
+
+	return 0;
+}
 
