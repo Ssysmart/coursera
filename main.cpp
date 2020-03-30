aa32012dd38688aa9de7c7f06881193da8f2ab36
Ssysmart <58028050+Ssysmart@users.noreply.github.com>
2020-03-30 16:43:04 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1726,8 +1726,192 @@ int main() {
 }
 
 
+描述
+写一个MyString 类，使得下面程序的输出结果是：
+
+1. abcd-efgh-abcd-
+
+2. abcd-
+
+3.
+
+4. abcd-efgh-
+
+5. efgh-
+
+6. c
+
+7. abcd-
+
+8. ijAl-
+
+9. ijAl-mnop
+
+10. qrst-abcd-
+
+11. abcd-qrst-abcd- uvw xyz
+
+about
+
+big
+
+me
+
+take
+
+abcd
+
+qrst-abcd-
+
+要求：MyString类必须是从C++的标准类string类派生而来。提示1：如果将程序中所有 "MyString" 用"string" 替换，那么题目的程序中除了最后两条语句编译无法通过外，其他语句都没有问题，而且输出和前面给的结果吻合。也就是说，MyString类对 string类的功能扩充只体现在最后两条语句上面。提示2: string类有一个成员函数 string substr(int start,int length); 能够求从 start位置开始，长度为length的子串
+
+程序：
 
 
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+using namespace std;
+// 在此处补充你的代码
+class MyString {
+	char* sValue;
+public:
+	MyString() { sValue = NULL; }
+	MyString(const char* a) {
+		int n = strlen(a);
+		sValue = new char[n + 1];
+		int i;
+		for (i = 0; i < n; i++) {
+			sValue[i] = a[i];
+		}
+		sValue[i] = '\0';
+	}
+
+	MyString operator+(MyString a) {
+		MyString m;
+		m.sValue = new char[(strlen(sValue) + strlen(a.sValue) + 1)];
+		strcpy(m.sValue, sValue);
+		strcat(m.sValue, a.sValue);
+		return m;
+	}
+	friend ostream& operator<<(ostream& a, const MyString& b);
+	char& operator[](int a) { return sValue[a]; }
+	MyString(string a) {
+		if (sValue)
+			delete[]sValue;
+		else {
+			sValue = new char[(strlen(a.c_str()))];
+			strcpy(sValue, a.c_str());
+		}
+
+	}
+	MyString operator+=(const char* a) {
+		int n;
+		n = strlen(a) + strlen(sValue);
+		char* p = new char[n + 1];
+		strcpy(p, sValue);
+		strcat(p, a);
+		delete[]sValue;
+		sValue = p;
+		return *this;
+	}
+	friend MyString operator+(const char*, MyString&);
+	MyString operator+(const char* a) {
+		char* p = new char[strlen(a) + strlen(sValue) + 1];
+		strcpy(p, sValue);
+		strcat(p, a);
+		if (sValue) { delete[]sValue; }
+		sValue = p;
+		return *this;
+	}
+	bool operator<(MyString& a) {
+		int i = strcmp(sValue, a.sValue);
+		if (i < 0) {
+			return 1;
+		}
+		else
+			return 0;
+	}
+	bool operator==(MyString& a) {
+		int i = strcmp(sValue, a.sValue);
+		if (i == 0) {
+			return 1;
+		}
+		else
+			return 0;
+	}
+	bool operator>(MyString& a) {
+		int i = strcmp(sValue, a.sValue);
+		if (i > 0) {
+			return 1;
+		}
+		else
+			return 0;
+	}
+
+
+    MyString operator()(int i, int j) {
+        MyString res;
+        res.sValue = new char[j];
+        int start=i;
+        int idx = 0;
+        for (idx=0; idx < j; idx++)
+            res.sValue[idx] = sValue[start++];
+        res.sValue[idx] = '\0';
+        return res;
+    }
+};
+MyString operator+(const char* a, MyString& b) {
+	MyString p;
+	p.sValue = new char[strlen(a) + strlen(b.sValue) + 1];
+	strcpy(p.sValue, a);
+	strcat(p.sValue, b.sValue);
+	return p;
+}
+ostream& operator<<(ostream& a, const MyString& b) {
+	if (b.sValue == NULL)
+		return a;
+	else {
+		a << b.sValue;
+		return a;
+	}
+}
+int CompareString(const void* e1, const void* e2) {
+	MyString* s1 = (MyString*)e1;
+	MyString* s2 = (MyString*)e2;
+	if (*s1 < *s2) return -1;
+	else if (*s1 == *s2) return 0;
+	else if (*s1 > * s2) return 1;
+}
+int main() {
+	MyString s1("abcd-"), s2, s3("efgh-"), s4(s1);
+	MyString SArray[4] = { "big","me","about","take" };
+	cout << "1. " << s1 << s2 << s3 << s4 << endl;
+	s4 = s3; s3 = s1 + s3;
+	cout << "2. " << s1 << endl;
+	cout << "3. " << s2 << endl;
+	cout << "4. " << s3 << endl;
+	cout << "5. " << s4 << endl;
+	cout << "6. " << s1[2] << endl;
+	s2 = s1; s1 = "ijkl-";
+	s1[2] = 'A';
+	cout << "7. " << s2 << endl;
+	cout << "8. " << s1 << endl;
+	s1 += "mnop";
+	cout << "9. " << s1 << endl;
+	s4 = "qrst-" + s2;
+	cout << "10. " << s4 << endl;
+	s1 = s2 + s4 + " uvw " + "xyz";
+	cout << "11. " << s1 << endl;
+	qsort(SArray, 4, sizeof(MyString), CompareString);
+	for( int i = 0;i < 4;++i )
+	        cout << SArray[i] << endl;
+	//输出s1从下标0开始长度为4的子串
+	    cout << s1(0,4) << endl;
+	    //输出s1从下标为5开始长度为10的子串
+	    cout << s1(5,10) << endl;
+	return 0;
+}
 
 
 
