a7fa18837f83620a95677b010b8f7daac030eef5
Ssysmart <58028050+Ssysmart@users.noreply.github.com>
2020-04-07 16:25:20 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1913,5 +1913,498 @@ int main() {
 	return 0;
 }
 
+编程题＃2： 魔兽世界之二：装备
 
+来源: POJ (Coursera声明：在POJ上完成的习题将不会计入Coursera的最后成绩。)
+
+注意： 总时间限制: 1000ms 内存限制: 65536kB
+
+描述
+
+魔兽世界的西面是红魔军的司令部，东面是蓝魔军的司令部。两个司令部之间是依次排列的若干城市。 
+
+红司令部，City 1，City 2，……，City n，蓝司令部
+
+两军的司令部都会制造武士。武士一共有 dragon 、ninja、iceman、lion、wolf 五种。每种武士都有编号、生命值这两种属性。 
+
+有的武士可以拥有武器。武器有三种，sword, bomb,和arrow，编号分别为0,1,2。
+
+双方的武士编号都是从1开始计算。红方制造出来的第 n 个武士，编号就是n。同样，蓝方制造出来的第 n 个武士，编号也是n。 
+
+不同的武士有不同的特点。
+
+dragon 可以拥有一件武器。编号为n的dragon降生时即获得编号为 n%3 的武器。dragon还有“士气”这个属性，是个浮点数，其值为它降生后其司令部剩余生命元的数量除以造dragon所需的生命元数量。
+
+ninjia可以拥有两件武器。编号为n的ninjia降生时即获得编号为 n%3 和 (n+1)%3的武器。
+
+iceman有一件武器。编号为n的iceman降生时即获得编号为 n%3 的武器。
+
+lion 有“忠诚度”这个属性，其值等于它降生后其司令部剩余生命元的数目。
+
+wolf没特点。
+
+请注意，在以后的题目里，武士的士气，生命值，忠诚度在其生存期间都可能发生变化，都有作用，武士手中的武器随着使用攻击力也会发生变化。
+
+武士在刚降生的时候有一个生命值。 
+
+在每个整点，双方的司令部中各有一个武士降生。 
+
+红方司令部按照 iceman、lion、wolf、ninja、dragon 的顺序循环制造武士。 
+
+蓝方司令部按照 lion、dragon、ninja、iceman、wolf 的顺序循环制造武士。 
+
+制造武士需要生命元。 
+
+制造一个初始生命值为 m 的武士，司令部中的生命元就要减少 m 个。 
+
+如果司令部中的生命元不足以制造某个按顺序应该制造的武士，那么司令部就试图制造下一个。如果所有武士都不能制造了，则司令部停止制造武士。
+
+给定一个时间，和双方司令部的初始生命元数目，要求你将从0点0分开始到双方司令部停止制造武士为止的所有事件按顺序输出。
+
+一共有两种事件，其对应的输出样例如下： 
+
+1) 武士降生 
+
+输出样例： 004 blue lion 5 born with strength 5,2 lion in red headquarter
+
+表示在 4点整，编号为5的蓝魔lion武士降生，它降生时生命值为5,降生后蓝魔司令部里共有2个lion武士。(为简单起见，不考虑单词的复数形式)注意，每制造出一个新的武士，都要输出此时司令部里共有多少个该种武士。
+
+如果造出的是dragon，那么还要输出一行，例：
+
+It has a arrow,and it's morale is 23.34
+
+表示该dragon降生时得到了arrow,其士气是23.34（为简单起见，本题中arrow前面的冠词用a,不用an，士气精确到小数点后面2位，四舍五入）
+
+如果造出的是ninjia，那么还要输出一行，例：
+
+It has a bomb and a arrow
+
+表示该ninjia降生时得到了bomb和arrow。
+
+如果造出的是iceman，那么还要输出一行，例：
+
+It has a sword
+
+表示该iceman降生时得到了sword。
+
+如果造出的是lion，那么还要输出一行，例：
+
+It's loyalty is 24
+
+表示该lion降生时的忠诚度是24。
+
+2) 司令部停止制造武士
+
+输出样例： 010 red headquarter stops making warriors
+
+表示在 10点整，红方司令部停止制造武士
+
+输出事件时： 
+
+首先按时间顺序输出； 
+
+同一时间发生的事件，先输出红司令部的，再输出蓝司令部的。
+
+
+输入
+
+第一行是一个整数,代表测试数据组数。
+
+每组测试数据共两行。 
+
+第一行，一个整数M。其含义为： 每个司令部一开始都有M个生命元( 1 <= M <= 10000) 
+
+第二行：五个整数，依次是 dragon 、ninja、iceman、lion、wolf 的初始生命值。它们都大于0小于等于10000
+
+
+输出
+
+对每组测试数据，要求输出从0时0分开始，到双方司令部都停止制造武士为止的所有事件。
+
+对每组测试数据，首先输出“Case:n" n是测试数据的编号，从1开始 
+
+接下来按恰当的顺序和格式输出所有事件。每个事件都以事件发生的时间开头，时间以小时为单位，有三位。
+
+
+//魔兽世界World of Warcraft
+#include <iostream>
+#include <iomanip>
+#include <stdio.h>
+#include <string>
+#include<cstring>
+using namespace std;
+
+class Charactor {
+protected:
+	string name;
+	int borntime;
+	int charactor_life;
+public:
+	Charactor(const string, int, int);
+	~Charactor() {};
+	const char* return_name() {
+		return (name.c_str());
+	}
+	int return_borntime() {
+		return borntime;
+	}
+	int return_charactor_life() {
+		return charactor_life;
+	}
+};
+
+struct s_life {
+	string species[5] = { "dragon","ninja","iceman","lion","wolf" };
+	int species_life[5] = { 0 };
+};
+
+class blueHeadquarter;
+class  redHeadquarter {
+	static int red_ID;
+	static float red_Headquarter_life;//大本营生命值
+	static int red_life[5];
+	static int red_num_of_charactor[5];
+
+public:
+	redHeadquarter() {};
+	~redHeadquarter() {};
+	static string red[5];
+	static void assign_life(int a, int b) {
+		red_life[a] = b;
+	}
+	friend class Charactor;
+	friend void c_in(redHeadquarter&, blueHeadquarter&, s_life&);
+	friend void rearrange_order(int, s_life);
+	float return_red_Headquarter_life() {
+		return red_Headquarter_life;
+	}
+	int* return_red_life(int a) {
+		return &red_life[a];
+	}
+	void red_manufacturing(int a) {
+		red_Headquarter_life -= red_life[a];
+	}
+	int& return_red_ID() {
+		return red_ID;
+	}
+	int* return_red_num_of_charactor() {
+		return red_num_of_charactor;
+	}
+	friend class dragon;
+	friend class lion;
+	friend void details(string, bool);
+	friend void reset();
+};
+int redHeadquarter::red_ID = 0;
+int redHeadquarter::red_life[5] = { 0 };
+int redHeadquarter::red_num_of_charactor[5] = { 0 };
+string redHeadquarter::red[5] = { "iceman", "lion", "wolf", "ninja", "dragon" };
+float redHeadquarter::red_Headquarter_life = 0;
+
+class blueHeadquarter {
+	static int blue_ID;
+	static float blue_Headquarter_life;//大本营生命值
+	static int blue_life[5];
+	static int blue_num_of_charactor[5];
+public:
+	blueHeadquarter() {};
+	~blueHeadquarter() {};
+	static string blue[5];
+	static void assign_life(int a, int b) {
+		blue_life[a] = b;
+	}
+	friend class Charactor;
+	friend void c_in(redHeadquarter&, blueHeadquarter&, s_life&);
+	friend void rearrange_order(int, s_life);
+	float return_blue_Headquarter_life() {
+		return blue_Headquarter_life;
+	}
+	int* return_blue_life(int a) {
+		return &blue_life[a];
+	}
+	void blue_manufacturing(int a) {
+		blue_Headquarter_life -= blue_life[a];
+	}
+	int& return_blue_ID() {
+		return blue_ID;
+	}
+	int* return_blue_num_of_charactor() {
+		return blue_num_of_charactor;
+	}
+	friend class dragon;
+	friend class lion;
+	friend void details(string, bool);
+	friend void reset();
+
+};
+int blueHeadquarter::blue_num_of_charactor[5] = { 0 };
+int blueHeadquarter::blue_ID = 0;
+float blueHeadquarter::blue_Headquarter_life = 0;
+int blueHeadquarter::blue_life[5] = { 0 };
+string blueHeadquarter::blue[5] = { "lion","dragon","ninja","iceman","wolf" };
+
+void reset() {
+	for (int i = 0; i < 5; i++) {
+		redHeadquarter::red_num_of_charactor[i] = 0;
+		blueHeadquarter::blue_num_of_charactor[i] = 0;
+	}
+	redHeadquarter::red_ID = 0;
+	blueHeadquarter::blue_ID = 0;
+
+}
+
+
+
+Charactor::Charactor(const string name_, int life, int time) {
+	name = name_;
+	borntime = time;
+	charactor_life = life;
+}
+bool Whether_stop(int a, const int* b) {
+	float tmp; redHeadquarter r; blueHeadquarter blue;
+	if (a == 1)
+		tmp = r.return_red_Headquarter_life();
+	else
+		tmp = blue.return_blue_Headquarter_life();
+	for (int i = 0; i < 5; i++) {
+		if (tmp >= *b++)
+			return true;
+	}
+	return false;
+}
+
+void c_in(redHeadquarter& a, blueHeadquarter& b, s_life& c) {
+	float life;
+	cin >> life;
+	b.blue_Headquarter_life = life;
+	a.red_Headquarter_life = life;
+	for (int i = 0; i < 5; i++) {
+		cin >> c.species_life[i];
+	}
+}
+
+void rearrange_order(int a, s_life b) {
+	if (a) {
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				if (strcmp((&b.species[j])->c_str(), (&redHeadquarter::red[i])->c_str()) == 0)
+					redHeadquarter::assign_life(i, b.species_life[j]);
+			}
+		}
+	}
+	else {
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				if (strcmp((&b.species[j])->c_str(), (&blueHeadquarter::blue[i])->c_str()) == 0)
+					blueHeadquarter::assign_life(i, b.species_life[j]);
+			}
+		}
+	}
+}
+void Jump_or_not(string* a, float b, int* c, int& d) {
+	if (b < *c) {
+		for (int i = 0; i < 5; i++) {
+			if (d == 4) {
+				if (b >= *c)
+					break;
+				else { c -= 4; d = 0; a -= 4; }
+			}
+			else {
+				if (b >= *c)
+					break;
+				else { d++; c++; a++; }
+			}
+		}
+	}
+}
+int num_of_charactor(string a, string* b, int array[]) {
+	for (int i = 0; i < 5; i++) {
+		if (strcmp(a.c_str(), (b[i]).c_str()) == 0) {
+			array[i] += 1;
+			return array[i];
+		}
+	}
+}
+
+class weapon {
+	static string wuqi[3];
+public:
+	weapon() {
+	}
+	friend void c_out(string s);
+};
+string weapon::wuqi[3] = { "sword","bomb","arrow" };
+class dragon :public weapon {
+	static int weapon_d;
+	static float morale;
+public:
+	dragon(int, float);
+	friend void c_out(string);
+};
+int dragon::weapon_d = 0;
+float dragon::morale = 0;
+dragon::dragon(int n, float m) {
+	weapon_d = n % 3;
+	morale = m / redHeadquarter::red_life[4];//友元类访问静态私有成员
+}
+
+
+class ninja :public weapon {
+	static int weapon_x;
+	static int weapon_y;
+public:
+	ninja(int);
+	friend void c_out(string);
+};
+int ninja::weapon_x = 0;
+int ninja::weapon_y = 0;
+ninja::ninja(int n) {
+	weapon_x = n % 3;
+	weapon_y = (n + 1) % 3;
+}
+class iceman :public weapon {
+	static int weapon;
+public:
+	iceman(int);
+	friend void c_out(string);
+};
+int iceman::weapon = 0;
+iceman::iceman(int n) {
+	weapon = n % 3;
+}
+class lion :public weapon {
+	static int loyalty;
+public:
+	lion(string);
+	friend void c_out(string);
+};
+int lion::loyalty = 0;
+lion::lion(string h) {
+	if (!strcmp(h.c_str(), "red"))
+		loyalty = (int)redHeadquarter::red_Headquarter_life;
+	else
+		loyalty = (int)blueHeadquarter::blue_Headquarter_life;
+}
+class wolf :public weapon {
+
+};
+void c_out(string);
+void details(string s, bool b) {
+	if (!strcmp(s.c_str(), "dragon")) {
+		if (b == 1)
+			dragon d(redHeadquarter::red_ID, redHeadquarter::red_Headquarter_life);//有元函数可以访问静态私有成员
+		else
+			dragon d(blueHeadquarter::blue_ID, blueHeadquarter::blue_Headquarter_life);
+		c_out("dragon");
+	}
+	else if (!strcmp(s.c_str(), "ninja")) {
+		if (b == 1)
+			ninja n(redHeadquarter::red_ID);
+		else
+			ninja n(blueHeadquarter::blue_ID);
+		c_out("ninja");
+	}
+	else if (!strcmp(s.c_str(), "iceman")) {
+		if (b == 1)
+			iceman i(redHeadquarter::red_ID);
+		else
+			iceman i(blueHeadquarter::blue_ID);
+		c_out("iceman");
+	}
+	else if (!strcmp(s.c_str(), "lion")) {
+		if (b == 1)
+			lion l("red");
+		else
+			lion l("blue");
+		c_out("lion");
+	}
+}
+void c_out(string s) {
+	if (!strcmp(s.c_str(), "dragon")) {
+		printf("It has a %s and it's morale is %.2f\n", weapon::wuqi[dragon::weapon_d].c_str(), dragon::morale);
+	}
+	else if (!strcmp(s.c_str(), "ninja")) {
+		printf("It has a %s and a %s\n", weapon::wuqi[ninja::weapon_x].c_str(), weapon::wuqi[ninja::weapon_y].c_str());
+	}
+	else if (!strcmp(s.c_str(), "iceman")) {
+		printf("It has a %s\n", weapon::wuqi[iceman::weapon].c_str());
+	}
+	else if (!strcmp(s.c_str(), "lion")) {
+		printf("It's loyalty is %d\n", lion::loyalty);
+	}
+}
+int main() {
+
+	int num_of_case;
+	cin >> num_of_case;
+	int case_n = 0;
+	while (num_of_case > 0) {
+		s_life object; int borntime = 0, red_i = 0, blue_i = 0; bool red_flag = 0, blue_flag = 0;
+		redHeadquarter red;
+		blueHeadquarter blue;
+		reset();
+		string* c1 = red.red;
+		string* c2 = blue.blue;
+
+		c_in(red, blue, object);
+		rearrange_order(1, object);
+		rearrange_order(0, object);
+		printf("Case:%u\n", ++case_n);
+
+
+		while (1) {
+			int record = 0;
+			if ((!Whether_stop(1, object.species_life) && !red_flag)) {
+				cout << setw(3) << setfill('0') << borntime << ' ' << "red headquarter stops making warriors" << endl;
+				red_flag = 1;
+			}
+			if (Whether_stop(1, object.species_life)) {
+				Jump_or_not(redHeadquarter::red, red.return_red_Headquarter_life(), red.return_red_life(red_i), red_i);
+				Charactor* pred = new Charactor(redHeadquarter::red[red_i], *red.return_red_life(red_i), borntime);
+				red.red_manufacturing(red_i);
+				printf("%03d %s %s %d born with strength %d,%d %s in %s headquarter\n",
+					pred->return_borntime(), "red", pred->return_name(), ++red.return_red_ID(), pred->return_charactor_life(),
+					num_of_charactor(red.red[red_i], red.red, red.return_red_num_of_charactor()), pred->return_name(), "red");
+				details(redHeadquarter::red[red_i], 1);
+				if (strcmp((*c1).c_str(), "dragon"))
+					c1 = red.red;
+				delete pred;
+				record += 1;
+				if (red_i < 4)
+					red_i++;
+				else
+					red_i = 0;
+			}
+			if ((!Whether_stop(0, object.species_life)) && !blue_flag) {
+				cout << setw(3) << setfill('0') << borntime << ' ' << "blue headquarter stops making warriors" << endl;
+				blue_flag = 1;
+			}
+			if (Whether_stop(0, object.species_life)) {
+				Jump_or_not(blueHeadquarter::blue, blue.return_blue_Headquarter_life(), blue.return_blue_life(blue_i), blue_i);
+				Charactor* pblue = new Charactor(blueHeadquarter::blue[blue_i], *blue.return_blue_life(blue_i), borntime);
+				blue.blue_manufacturing(blue_i);
+				printf("%03d %s %s %d born with strength %d,%d %s in %s headquarter\n",
+					pblue->return_borntime(), "blue", pblue->return_name(), ++blue.return_blue_ID(), pblue->return_charactor_life(),
+					num_of_charactor(blue.blue[blue_i], blue.blue, blue.return_blue_num_of_charactor()), pblue->return_name(), "blue");
+				details(blueHeadquarter::blue[blue_i], 0);
+				if (strcmp((*c2).c_str(), "Wolf"))
+					c2 = blue.blue;
+				delete pblue;
+				record += 1;
+				if (blue_i < 4) {
+					blue_i++;
+				}
+				else
+					blue_i = 0;
+			}
+			if (record == 0)
+				break;
+			borntime++;
+
+		}
+		num_of_case--;
+
+	}
+	return 0;
+}
 
