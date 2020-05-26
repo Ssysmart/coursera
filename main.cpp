c47aa187939f42c5c862af6e0009d1fa75633981
Ssysmart <58028050+Ssysmart@users.noreply.github.com>
2020-05-26 11:11:53 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2408,3 +2408,211 @@ int main() {
 	return 0;
 }
 
+
+给定n个字符串（从1开始编号），每个字符串中的字符位置从0开始编号，长度为1-500，现有如下若干操作：
+
+copy N X L：取出第N个字符串第X个字符开始的长度为L的字符串。
+
+add S1 S2：判断S1，S2是否为0-99999之间的整数，若是则将其转化为整数做加法，若不是，则作字符串加法，返回的值为一字符串。
+
+find S N：在第N个字符串中从左开始找寻S字符串，返回其第一次出现的位置，若没有找到，返回字符串的长度。
+
+rfind S N：在第N个字符串中从右开始找寻S字符串，返回其第一次出现的位置，若没有找到，返回字符串的长度。
+
+insert S N X：在第N个字符串的第X个字符位置中插入S字符串。
+
+reset S N：将第N个字符串变为S。
+
+print N：打印输出第N个字符串。
+
+printall：打印输出所有字符串。
+
+over：结束操作。
+
+其中N，X，L可由find与rfind操作表达式构成，S，S1，S2可由copy与add操作表达式构成。
+
+输入
+
+第一行为一个整数n（n在1-20之间）
+
+接下来n行为n个字符串，字符串不包含空格及操作命令等。
+
+接下来若干行为一系列操作，直到over结束。
+
+输出
+
+根据操作提示输出对应字符串。
+
+#include <iostream>
+#include <string>
+#include<stdlib.h>
+#include<cstdlib>
+
+using namespace std;
+string str[22];
+string Myreset();
+void getString(string&);
+void getInt(int&);
+string Mycopy();
+string MyAdd();
+int MyRfind();
+int Myfind();
+void Getstring(string&, int&);
+string MyInsert() {
+	string s;
+	int n, x;
+	getString(s);
+	getInt(n);
+	getInt(x);
+	return(str[n].insert(x, s));
+
+}
+void Myitoa(int m, char* const str, int& i) {
+	if (m >= 0 && m <= 9) {
+		str[i++] = (char)(m + 48);
+	}
+	else {
+		Myitoa(m / 10, str, i);
+		str[i++] = (char)(m % 10 + 48);
+	}
+}
+int Myatoi(const char* str) {
+	if (str == NULL) {
+		return -1;
+	}
+	else {
+		return (int)*str - 48;
+	}
+}
+void getInt(int& n) {
+	string s;
+	cin >> s;
+	if (s == "find") {
+		n = Myfind();
+	}
+	else if (s == "rfind") {
+		n = MyRfind();
+	}
+	else
+		n = Myatoi(s.c_str());
+}
+int Myfind() {
+	string s;
+	getString(s);
+	int n;
+	getInt(n);
+	int value;
+	value = str[n].find(s.c_str());
+	if (value == string::npos)
+		return s.length();
+	else
+		return value;
+}
+int MyRfind() {
+	string s;
+	getString(s);
+	int n;
+	getInt(n);
+	int value = str[n].rfind(s.c_str());
+	if (value == string::npos)
+		return s.length();
+	else
+		return value;
+}
+void getString(string& s) {
+	string Instruct;
+	cin >> Instruct;
+	if (Instruct == "copy") {
+		s = Mycopy();
+	}
+	else if (Instruct == "add") {
+		s = MyAdd();
+	}
+	else
+		s = Instruct;
+}
+
+string MyAdd() {
+	string s1, s2;
+	int m = -1, n = -1;
+	Getstring(s1, m);
+	Getstring(s2, n);
+	if (m == -1 || n == -1) {
+		return(s1 + s2);
+	}
+	else {
+		m += n;
+		char str[7];
+		int i = 0;
+		Myitoa(m, str, i);
+		str[i] = '\0';
+		return (string)str;
+	}
+}
+
+
+void Getstring(string& s, int& m) {
+	getString(s);
+	unsigned int i = 0;
+	for (m = 0; i < s.length(); i++) {
+		if ( s.at(i) >= '0' && s.at(i) <= '9') {
+			m = m * 10 + s.at(i) - '0';
+		}
+		else
+			break;
+	}
+	if (i != s.length()  || m>99999) {
+		m = -1;
+	}
+}
+string Mycopy() {
+	int n, x, l;
+	getInt(n);
+	getInt(x);
+	getInt(l);
+	return(str[n].substr(x, l));
+}
+string Myreset() {
+	string s;
+	int n;
+	getString(s);
+	getInt(n);
+	return (str[n].assign(s));
+
+}
+int main() {
+	int N;
+	cin >> N;
+	for (int i = 1; i <= N; i++) {
+		cin >> str[i];
+	}
+	string instruct;
+	while (cin >> instruct) {
+		if (instruct == "over")
+			break;
+		switch (instruct.at(1)) {
+		case('n'):
+			MyInsert(); break;
+		case ('e'):
+			Myreset(); break;
+		case('r'):if (instruct == "print") {
+			int n;
+			cin >> n;
+			cout << str[n] << endl;
+		}
+				 else {
+			for (int i = 1; i <= N; i++) {
+				cout << str[i] << endl;
+			}
+		}
+		default:
+			;
+		}
+
+	}
+
+
+
+
+	return 0;
+}
