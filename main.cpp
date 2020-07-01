c7dde465ba1ea244f33aeac470eadb7d56b61c1d
Ssysmart <58028050+Ssysmart@users.noreply.github.com>
2020-07-01 18:20:15 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2616,3 +2616,64 @@ int main() {
 
 	return 0;
 }
+
+描述
+
+为了迎接08年的奥运会，让大家更加了解各种格斗运动，facer新开了一家热血格斗场。格斗场实行会员制，但是新来的会员不需要交入会费，而只要同一名老会员打一场表演赛，证明自己的实力。
+
+我们假设格斗的实力可以用一个正整数表示，成为实力值。另外，每个人都有一个唯一的id，也是一个正整数。为了使得比赛更好看，每一个新队员都会选择与他实力最为接近的人比赛，即比赛双方的实力值之差的绝对值越小越好，如果有两个人的实力值与他差别相同，则他会选择比他弱的那个（显然，虐人必被虐好）。
+
+不幸的是，Facer一不小心把比赛记录弄丢了，但是他还保留着会员的注册记录。现在请你帮facer恢复比赛纪录，按照时间顺序依次输出每场比赛双方的id。
+
+
+输入
+
+第一行一个数n(0 < n <=100000)，表示格斗场新来的会员数（不包括facer）。以后n行每一行两个数，按照入会的时间给出会员的id和实力值。一开始，facer就算是会员，id为1，实力值1000000000。输入保证两人的实力值不同。
+
+
+输出
+
+N行，每行两个数，为每场比赛双方的id，新手的id写在前面。
+
+#include <iostream>
+#include <string>
+#include<map>
+using namespace std;
+
+int main() {
+	int newer;
+	cin >> newer;
+	typedef map<int, int> mem;
+
+	mem huiyuan;
+	mem::iterator myself;
+	mem::iterator duishou_1;
+	mem::iterator duishou_2;
+	mem::iterator result;
+	huiyuan.insert(mem::value_type(1000000000,1));
+	for (; newer > 0; newer--) {
+		int new_id, new_power,ind;
+		cin >> new_id >> new_power;
+		myself = huiyuan.insert(make_pair(new_power, new_id)).first;
+		duishou_1 = myself; duishou_2 = myself; result = myself;
+		if (++result == huiyuan.end()) {
+			result = duishou_1--;
+		}
+		else if (--result == huiyuan.begin()) {
+			result++;
+		}
+		else if (result != huiyuan.begin() && result != huiyuan.end()) {
+			--duishou_1; ++duishou_2;
+			if (abs(duishou_1->first - myself->first) < abs(duishou_2->first - myself->first))
+				result = duishou_1;
+			else if (abs(duishou_1->first - myself->first) > abs(duishou_2->first - myself->first)) { result = duishou_2; }
+			if (abs(duishou_1->first - myself->first) == abs(duishou_2->first - myself->first))
+				result = duishou_1;
+		}
+		cout << new_id << " " << result->second << endl;
+		
+	}
+
+
+	return 0;
+}
